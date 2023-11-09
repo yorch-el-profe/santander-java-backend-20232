e27bd5ede9bbdb864c9db640f12dfbeb88559985
Jorge Ramón <jorge.ramon@autodesk.com>
2023-11-08 20:17:40 -0600

    Sesion 01: Ejemplo básico de Spring Boot

---
+++ sesion-01-introduccion/01-introduccion/src/main/java/org/bedu/introduccion/IntroduccionApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.introduccion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroduccionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroduccionApplication.class, args);
+	}
+
+}
