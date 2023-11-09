965b17fe3b38779577c1c853e7707089459ab2e2
Jorge Ramón <jorge.ramon@autodesk.com>
2023-11-08 20:54:57 -0600

    Sesion 01: Completado ejemplo de suma de números con Scanner

--- sesion-01-introduccion/01-introduccion/src/main/java/org/bedu/introduccion/IntroduccionApplication.java
+++ sesion-01-introduccion/01-introduccion/src/main/java/org/bedu/introduccion/IntroduccionApplication.java
@@ -1,13 +1,32 @@
 package org.bedu.introduccion;
 
+import java.util.Scanner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroduccionApplication {
+public class IntroduccionApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroduccionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Ingresa el primer número: ");
+		int a = sc.nextInt();
+		sc.nextLine();
+
+		System.out.println("Ingresa el segundo número:");
+		int b = sc.nextInt();
+		sc.nextLine();
+
+		int resultado = a + b;
+
+		System.out.println("Resultado: " + resultado);
+		sc.close();
+	}
 }
