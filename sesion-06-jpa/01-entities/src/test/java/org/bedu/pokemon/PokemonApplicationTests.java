52458be8ac1d7ec4d8dc51d16d11d56e23bafb78
Jorge Ramón <jorge.ramon@autodesk.com>
2023-11-29 20:01:28 -0600

    Agregando código base del API de Pokemon

---
+++ sesion-06-jpa/01-entities/src/test/java/org/bedu/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bedu.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
