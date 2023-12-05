e676e887b90d8d5666d58e6b59eadf40a17665c3
Jorge Ramón <jorge.ramon@autodesk.com>
2023-12-04 21:21:56 -0600

    Sesion 07: Endpoint para crear artistas

--- sesion-07-repaso/01-spotify-api/src/main/java/org/bedu/spotify/controller/ArtistController.java
+++ sesion-07-repaso/01-spotify-api/src/main/java/org/bedu/spotify/controller/ArtistController.java
@@ -3,13 +3,18 @@ package org.bedu.spotify.controller;
 import java.util.List;
 
 import org.bedu.spotify.dto.ArtistDTO;
+import org.bedu.spotify.dto.CreateArtistDTO;
 import org.bedu.spotify.service.ArtistService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("artists")
@@ -22,5 +27,11 @@ public class ArtistController {
     @ResponseStatus(HttpStatus.OK)
     public List<ArtistDTO> findAll() {
         return service.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ArtistDTO save(@Valid @RequestBody CreateArtistDTO data) {
+        return service.save(data);
     }
 }
--- sesion-07-repaso/01-spotify-api/src/main/java/org/bedu/spotify/service/ArtistService.java
+++ sesion-07-repaso/01-spotify-api/src/main/java/org/bedu/spotify/service/ArtistService.java
@@ -3,7 +3,9 @@ package org.bedu.spotify.service;
 import java.util.List;
 
 import org.bedu.spotify.dto.ArtistDTO;
+import org.bedu.spotify.dto.CreateArtistDTO;
 import org.bedu.spotify.mapper.ArtistMapper;
+import org.bedu.spotify.model.Artist;
 import org.bedu.spotify.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +21,10 @@ public class ArtistService {
 
     public List<ArtistDTO> findAll() {
         return mapper.toDTO(repository.findAll());
+    }
+
+    public ArtistDTO save(CreateArtistDTO data) {
+        Artist entity = repository.save(mapper.toModel(data));
+        return mapper.toDTO(entity);
     }
 } 
